70525e06f75ae425810c5c7955dbb1c2cc4e7f62
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-02-01 22:37:12 +0700

    membuat dasboard dinamiys

--- app/Http/Controllers/DasboardController.php
+++ app/Http/Controllers/DasboardController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class DasboardController extends Controller
 {
+
+
+    public function __construct()
+    {
+        return $this->middleware(['auth']);
+    }
+
     public function index()
     {
-        return view('pages.dasboard');
+
+        $income = Transaction::where('transaction_status', 'SUCCESS')->sum('transaction_total');
+        $sales = Transaction::count();
+        $items = Transaction::orderBy('id', 'DESC')->take(5)->get();
+        $pie = [
+            'pending' => Transaction::where('transaction_status', 'PENDING')->count(),
+            'success' => Transaction::where('transaction_status', 'SUCCESS')->count(),
+            'failed' => Transaction::where('transaction_status', 'FAILED')->count(),
+        ];
+
+        return view('pages.dasboard')->with([
+            'income' => $income,
+            'sales' => $sales,
+            'items' => $items,
+            'pie' => $pie,
+        ]);
     }
 }
